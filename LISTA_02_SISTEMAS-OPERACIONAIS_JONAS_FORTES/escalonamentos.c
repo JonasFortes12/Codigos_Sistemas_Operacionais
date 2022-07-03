630f7ce8d90bae58620c81f2ff18da065a917bbd
JonasFortes <jonascforte@hotmail.com>
2022-07-03 15:45:34 -0300

    escalonamentos funcionando, mas problema de loop infinito no Round Robin

--- LISTA_02_SISTEMAS-OPERACIONAIS_JONAS_FORTES/escalonamentos.c
+++ LISTA_02_SISTEMAS-OPERACIONAIS_JONAS_FORTES/escalonamentos.c
@@ -111,7 +111,7 @@ Process* insertShortestProcessFirst(Process* prss, int id, int lenght, int order
     return prss;
 }
 
-/*Função para tornar uma lista circular, girar lista*/
+/*Funcao para tornar uma lista circular, girar lista*/
 Process* turnRound(Process* prss){
 
     Process* prssAux = prss;
@@ -124,8 +124,19 @@ Process* turnRound(Process* prss){
     return prssAux;
 }
 
-
+/*Funcoes de escalonamento */
 void schedulerFIFO(Process* prss){   
+
+    Process* prssAux = prss;
+
+    printf("________ESCALONAMENTO FIFO___________\n");
+    while (prssAux != NULL){
+        printf("Processo %c executando [%d ut]\n", prssAux->id, prssAux->length);
+        sleep(prssAux->length);
+        prssAux->length = 0;
+
+        prssAux = prssAux->next;
+    }
 }
 
 void schedulerSJF(Process* prss){
@@ -149,33 +160,34 @@ void schedulerSJF(Process* prss){
     for(Process* temp = shortests; temp != NULL; temp = temp->next){
 
         printf("Processo %c executando... [%d ut]\n", temp->id, temp->length);
-        sleep((temp->length)*1000);
+        sleep(temp->length);
     }
     freeProcess(shortests);
-
-    printf("________FIM SJF___________\n");
+    freeProcess(prss);
 
 }
 
 void schedulerRR(Process* prss, int quantum){  
-
+    
     Process* prssAux = turnRound(prss);
 
+    printf("________ESCALONAMENTO RR___________\n");
     while (prssAux != NULL){
-        
-        if(prssAux->length != 0 && prssAux->length <= quantum){
+    
+        if((prssAux->length != 0 ) && (prssAux->length <= quantum)){
 
-            printf("Processo %c executando... [%d ut]\n", prssAux->id, prssAux->length);
-            sleep((prssAux->length)*1000);
+            printf("Processo %c executando [%d ut] final\n", prssAux->id, prssAux->length);
+            sleep(prssAux->length);
             prssAux->length = 0;
+            
             
         }else if(prssAux->length > quantum){
 
-            printf("Processo %c executando... [%d ut]\n", prssAux->id, prssAux->length);
-            sleep((prssAux->length)*1000);
+            printf("Processo %c executando %dut. Tamanho: [%d ut]\n", prssAux->id, quantum, prssAux->length);
+            sleep(quantum);
             prssAux->length -= quantum ;
 
-        }else break;
+        }
 
         prssAux = prssAux->next;
     }
@@ -192,30 +204,34 @@ void main(){
     printf("Quantidade de processos na fila: ");
     scanf("%d",&numProcess);
 
-    for(int i = 1; i <= numProcess; i++){ //Laço para ler os valores
+    for(int i = 1; i <= numProcess; i++){
         printf("Digite um identificador para o %dº processo(char): ", i);
-        scanf(" %c",&id);//ERROR no codeblocks -> não executa o scanf
+        scanf(" %c",&id);
         printf("Digite a ordem de chegada do processo(int): ");
         scanf("%d",&order);
         printf("Digite o tempo de execucao do processo(int): ");
         scanf("%d",&lenght);
 
-        process = insertProcess(process, id, lenght, order); //Vai inserindo os processo na lista de processos
+        process = insertProcess(process, id, lenght, order);
         printf("\n");
     }
 
-    printf("[1] - SFJ\n");
-    printf("[2] - Round Robin\n");
+    printf("[1] - FIFO\n");
+    printf("[2] - SJF\n");
+    printf("[3] - Round Robin\n");
     printf("Qual algoritmo de escalonamento deseja: ");
     scanf("%d",&option);
 
     switch (option){
         case 1:
-            printProcess(process);
-            schedulerSJF(process);
+            schedulerFIFO(process);
             break;
         
         case 2:
+            schedulerSJF(process);
+            break;
+        
+        case 3:
             printf("quantum: ");scanf("%d",&quantum);
             schedulerRR(process, quantum);
             break;
@@ -229,3 +245,5 @@ void main(){
     printf("\n");
 
 }
+
+
---
+++ LISTA_02_SISTEMAS-OPERACIONAIS_JONAS_FORTES/item03.c
@@ -0,0 +1,75 @@
+/*
+author: Gabriel 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
+
+int numPrimo = 0; //Armazena o número de primos
+
+//Função para saber se o númeor é primo
+int ehPrimo(int n) {
+	int i, cont = 0;
+
+	for(i = 1; i <= n; i++) {
+		if (n % i == 0) {
+		cont++;
+		}
+	}
+
+	if (cont == 2) {
+		return 1; //quando for primo
+	} else {
+		return 0; //quando não for primo
+	}
+}
+
+//Função para contar a quantidade de primos de N/2 até 0
+void *qntPrimo1(void *num){
+	
+	int *n = (int *) num;
+
+	int cont; 
+	int contPrimo = 0; 
+	
+	for(cont = 2; cont <= (*n/2); cont++){
+		if(ehPrimo(cont) != 0){
+			contPrimo++;
+		}
+	}numPrimo = numPrimo + contPrimo;
+}
+
+//Função para contar a quantidade de primos de N até N/2 + 1
+void *qntPrimo2(void *num){
+	
+	int *n = (int *) num;
+
+	int cont;
+	int contPrimo = 0;
+	
+	for(cont = (*n/2)+1; cont <= *n; cont++){
+		if(ehPrimo(cont) != 0){
+			contPrimo++;
+		}
+	}numPrimo = numPrimo + contPrimo;
+}
+
+
+void main(){
+
+	int n;
+	pthread_t thread1, thread2;
+	printf("Digite um numero: ");
+	scanf("%d", &n);
+	
+	pthread_create(&thread1, NULL, qntPrimo1, &n);
+	pthread_create(&thread2, NULL, qntPrimo2, &n);
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+    
+	printf("Quantidade de numeros primos = %d\n", numPrimo);
+
+}
