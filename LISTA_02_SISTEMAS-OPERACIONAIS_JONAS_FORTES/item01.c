9b0723ee76977a9024406929dfe646e0920d6d6f
JonasFortes <jonascforte@hotmail.com>
2022-07-07 11:52:30 -0300

    item01 concertado

--- LISTA_02_SISTEMAS-OPERACIONAIS_JONAS_FORTES/item01.c
+++ LISTA_02_SISTEMAS-OPERACIONAIS_JONAS_FORTES/item01.c
@@ -3,45 +3,44 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <string.h>
-#define N 100 //quantidade threads
-
+#define N 10 //quantidade threads filhas
+ 
 pthread_t tid[N];
-
-
+pthread_t father;
+ 
+ 
 void* helloThread(void* arg){
-
+ 
     int num = *((int*)arg);
-
-    pthread_t id = pthread_self();
-
-    if(pthread_equal(id, tid[0])){
-        printf("Alo do Pai\n");
-        for(int j = 0; j<(0x0FFFFFFF); j++);//simular processamento
-        printf("Thal do Pai\n");
-       
-    }else{
-        printf("Alo do Filho [%d]\n", num);
-        for(int j = 0; j<(0x0FFFFFFF); j++);
-        printf("Thal do Filho [%d]\n", num);
-    }
-    
+ 
+    printf("Alo do Filho [%d]\n", num);
+    for(int j = 0; j<(0x0FFFFFFF); j++);
+    printf("Thal do Filho [%d]\n", num);
+ 
     free(arg);
     return NULL;
 }
-
-void main(){
-
+ 
+void main(void* arg){
+ 
+    printf("Alo do Pai\n");
+   
     for(int i = 0; i < N; i++){
-        
+       
         int* num = malloc(sizeof(int));
         *num = i;
-        
+       
         if (pthread_create( &(tid[i]), NULL, &helloThread, num) != 0)
             printf("\ncan't create thread[%d]", i);
     }
-
+ 
     for(int i = 0; i < N; i++){
         if (pthread_join(tid[i], NULL) != 0)
             printf("\ncan't join thread[%d]", i);
     }
+   
+    for(int j = 0; j<(0x0FFFFFFF); j++);
+    printf("Thal do Pai\n");
+ 
 }
+
